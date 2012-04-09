552f043d566eb41808d08902dc01418ed7762e5b
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2012-04-09 17:19:22 +0200

    Added logging for unrar script.

--- transmission_unrar.sh
+++ transmission_unrar.sh
@@ -7,23 +7,43 @@ SOURCE="/share/Qmultimedia/video/"
 DESTINATION="/share/Qmultimedia/video/"
 LIST="/share/Qdownload/transmission/extracted.txt"
 
+# Log dir
+LOGDIR="`dirname $0`/log"
+LOGFILE="${LOGDIR}/`basename $0`.log"
+
+# Create logs directory if it does not exist.
+if [ ! -d "$LOGDIR" ]; then
+  echo "Creating log dir: ${LOGDIR}"
+  mkdir $LOGDIR
+fi
+
+# Log content to log file
+function log {
+  LINE="`date +'%F %H:%M:%S'` $1"
+  echo $LINE
+  echo $LINE >> $LOGFILE
+}
+
 # Find all RARs in working directory.
+log "Starting job"
 for MYRAR in `find "$SOURCE" -name *.rar`
 do
    # Check if it's been decompressed before
    if grep -q $MYRAR $LIST
    then
-      echo "Already decompressed"
+      # Do nothing.
+      echo "Already decompressed" > /dev/null
 
    # If it hasn't be decompressed before, make a new folder and decompress it
    else
       # Add the name to the list so it will be skipped in the future
-      echo $MYRAR >>$LIST
+      echo $MYRAR >> $LIST
       
+      log "Decompressing $MYRAR"
       FOLDERNAME=`echo "${MYRAR:${#SOURCE}}"`
       FOLDERNAME=`echo $FOLDERNAME | sed 's:/.*::'`
       FINALADDRESS=$DESTINATION$FOLDERNAME
-      #Create directory only if it does not exist.
+      # Create directory only if it does not exist.
       if [ ! -d "$FINALADDRESS" ]; then
         mkdir $FINALADDRESS
       fi
@@ -31,5 +51,6 @@ do
    fi
 done
 
+log "Job ended"
 exit
 
