0fa0ac1cbe62571fca15718a26fc5d44a63808e3
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2012-04-16 21:58:38 +0200

    Added logging for subtitles script.

--- subtitles.sh
+++ subtitles.sh
@@ -9,5 +9,23 @@
 
 DESTINATION="/share/Qmultimedia/video/"
 
+# Log dir
+LOGDIR="`dirname $0`/log"
+LOGFILE="${LOGDIR}/`basename $0`.log"
+
+# Create logs directory if it does not exist.
+if [ ! -d "$LOGDIR" ]; then
+  echo "Creating log dir: ${LOGDIR}"
+  mkdir $LOGDIR
+fi
+
+# Log content to log file
+function log {
+  LINE="`date +'%F %H:%M:%S'` $1"
+  echo $LINE
+  echo $LINE >> $LOGFILE
+}
+
+log "Starting job"
 find $DESTINATION \( -name *.avi -o -name *.mkv -o -name *.mov -o -name *.mp4 \) -type f | grep -v 'sample' | xargs periscope -l en -l lt
 
