8d5b610515b1eb9aeac0c30e68bed73485bb118b
Povilas Balzaravicius Pawka <pavvka@gmail.com>
2012-09-29 18:55:32 +0300

    Clean unmounted directories.

--- settings.example.sh
+++ settings.example.sh
@@ -2,17 +2,16 @@
 #
 # Author: Povilas Balzaravičius <pavvka@gmail.com>
 #
-# Global configs file.
-
+# Global configs file. All global settings variables starts with underscore "_"
+# character to easier separate them.
 
 #
 # Feel free to change settings
 #
-SERVER="deathstar.local"
-MOUNTDIR="/home/pawka/Desktop/${SERVER}"
-
+_SERVER="deathstar.local"
+_MOUNTDIR="/home/pawka/Desktop/${_SERVER}"
 
 #
 # Do not change bellow.
 #
-ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+_ROOTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
--- tools/mount_nfs.sh
+++ tools/mount_nfs.sh
@@ -5,17 +5,43 @@
 # Mount all available network shares of server.
 #
 
-SERVER="deathstar.local"
-MOUNTDIR="/home/pawka/Desktop/${SERVER}"
+# Load settings
+. `dirname $0`/../settings.sh
 
-# Collect all available shares of server.
-SHARES=`showmount -e ${SERVER} | egrep "^/[A-Za-z ]+$" -o`
+function getMountedDirs {
+    echo `mount -l | grep ${_SERVER} | egrep "^([^ ]+)" -o | egrep "/.+" -o | tr " " "\n"`
+}
 
-for SHARE in $SHARES
+function getAvailableDirs {
+    echo `showmount -e ${_SERVER} | egrep "^/[A-Za-z ]+$" -o`
+}
+
+function getUnmountedDirs {
+    MOUNTED=`getMountedDirs`
+    UNMOUNTED=()
+    for DIR in `getAvailableDirs`
+    do
+        if [ 0 -eq `echo $MOUNTED | grep $DIR | wc -l` ]; then
+            echo $DIR
+        fi
+    done
+}
+
+function cleanUnmountedDirs {
+    find $_MOUNTDIR -maxdepth 1 -empty -type d -exec rmdir {} \;
+
+    # Remove mount directory if nothing is mounted.
+    if [[ 0 -eq `ls -1 $_MOUNTDIR | wc -l` ]]; then
+        rmdir $_MOUNTDIR
+    fi
+}
+
+for SHARE in `getAvailableDirs`
 do
-    DIR=`echo ${MOUNTDIR}$SHARE`
+    DIR=`echo ${_MOUNTDIR}$SHARE`
     if [ ! -f "$DIR" ]; then
         mkdir -p $DIR
     fi
-    sudo mount ${SERVER}:${SHARE} $DIR
+    sudo mount ${_SERVER}:${SHARE} $DIR
 done
+cleanUnmountedDirs
